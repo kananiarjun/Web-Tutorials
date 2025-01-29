97e2aa761301293dcd67f26a850943a60c6c6313
Arjun Kanani <arjunkanani2@gmail.com>
2025-01-29 20:03:15 +0530

    Delete PHP Practicals directory

--- PHP Practicals/Lab/Program-8/logout.php
+++
@@ -1,4 +0,0 @@
-<?php
-    setcookie("user", "abc@example.com", time() - 86400, "/");
-    setcookie("pass", "abc@example.com", time() - 86400, "/");
-    header("Location:http://localhost/laravel-tutorials/Program-8/");
--- PHP Practicals/Lab/Program-9/Employee.class.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once 'Person.class.php';
-class Employee extends Person{
-    public function __construct(){
-        echo "I am called from Employee Class";
-    }
-}
